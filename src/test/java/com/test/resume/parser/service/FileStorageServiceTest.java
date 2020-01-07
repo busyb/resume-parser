6d08e66f1b268f1cc82b4e05453c2815155946f9
HanchaoLi <seraphme1234@gmail.com>
2020-01-06 20:08:22 -0500

    RP-001: create test code for getMachineLearningResultsFromFile method

--- src/test/java/com/test/resume/parser/service/FileStorageServiceTest.java
+++ src/test/java/com/test/resume/parser/service/FileStorageServiceTest.java
@@ -1,14 +1,20 @@
 package com.test.resume.parser.service;
 
 import com.test.resume.parser.config.FileStorageProperties;
+import com.test.resume.parser.model.MLResult;
+import com.test.resume.parser.model.Result;
 import com.test.resume.parser.util.FileStorageHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -40,6 +46,24 @@ class FileStorageServiceTest {
         String response = service.storeFile(testFile, testKey);
 
         assertEquals("testing file name","test file",response);
+    }
+
+    @Test
+    void getMachineLearningResultsFromFile() throws IOException{
+        String timestamp = new Date().toString();
+        when(properties.getOutputDir()).thenReturn("");
+        File file = new File(getClass().getClassLoader().getResource("test").getFile());
+        when(helper.getFilePath(any(), any())).thenReturn(Paths.get(file.getAbsolutePath()));
+        MLResult response = service.getMachineLearningResultsFromFile(timestamp);
+        List<Result> resList = response.getResult();
+        for (int i  = 0; i < resList.size(); i++) {
+            assertEquals("name", "test0" + (i + 1), resList.get(i).getName());
+            assertEquals("outcome", "test0" + (i + 1), resList.get(i).getOutcome());
+        }
+//        response.getResult().stream().forEach(result -> {
+//            assertEquals("name", "test01", result.getName());
+//            assertEquals("outcome", "test0101", result.getOutcome());
+//        });
     }
 
 }
