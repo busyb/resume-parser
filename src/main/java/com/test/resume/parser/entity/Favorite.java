f07d5284e1cd0d39c45cd752c4c8e101f42b5952
HanchaoLi <seraphme1234@gmail.com>
2020-01-20 19:01:58 -0500

    create entity class for DB

---
+++ src/main/java/com/test/resume/parser/entity/Favorite.java
@@ -0,0 +1,22 @@
+package com.test.resume.parser.entity;
+
+public class Favorite {
+    int favoriteId;
+    int status;
+
+    public int getFavoriteId() {
+        return favoriteId;
+    }
+
+    public void setFavoriteId(int favoriteId) {
+        this.favoriteId = favoriteId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
